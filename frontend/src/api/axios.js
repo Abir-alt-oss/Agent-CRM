52b720a1148dc1a5789de298663f4f825f9798b6
Abir-alt-oss <ouelhaziabir.92@gmail.com>
2026-03-18 15:13:49 +0100

    update api backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    "https://agent-crm-1-9nqp.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
