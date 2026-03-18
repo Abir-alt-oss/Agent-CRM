e4831cd6d2e6573cd0e5e17c584b65b21056c53c
Abir-alt-oss <ouelhaziabir.92@gmail.com>
2026-03-18 22:38:03 +0100

    fix email sendgrid

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -13,7 +13,7 @@ const createTransporter = () =>
   nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
-    secure: true,
+    secure: false,
     auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
   });
 
